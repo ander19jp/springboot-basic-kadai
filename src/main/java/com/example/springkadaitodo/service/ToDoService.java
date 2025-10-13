88c62fe84ce6ddb60df9496dc736983f999d8853
ander19jp <ander19jp@gmail.com>
2025-10-13 14:37:43 +0900

    Initial commit: Spring Boot TODO application

---
+++ src/main/java/com/example/springkadaitodo/service/ToDoService.java
@@ -0,0 +1,25 @@
+package com.example.springkadaitodo.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.example.springkadaitodo.entity.ToDo;
+import com.example.springkadaitodo.repository.ToDoRepository;
+
+@Service
+public class ToDoService {
+	
+	private final ToDoRepository toDoRepository;
+	
+	// コンストラクタインジェクション
+	public ToDoService(ToDoRepository toDoRepository) {
+		this.toDoRepository = toDoRepository;
+	}
+	
+	
+	public List<ToDo>findAll(){
+		return toDoRepository.findAll();
+	}
+
+}
